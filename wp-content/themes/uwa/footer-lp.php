79be63ba55a5e93b1b170ac3504fd06502a284c6
Brian Maier Jr <brimaidesigns@gmail.com>
2017-01-25 14:16:23 -0500

    + web font loader

--- wp-content/themes/uwa/footer-lp.php
+++ wp-content/themes/uwa/footer-lp.php
@@ -20,6 +20,20 @@
             //]]>
         </script>
 
+				<script type="text/javascript">
+					WebFontConfig = {
+						google: { families: [ 'Open+Sans:400,400i,700,700i' ] }
+					};
+					(function() {
+						var wf = document.createElement('script');
+						wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+						wf.type = 'text/javascript';
+						wf.async = 'true';
+						var s = document.getElementsByTagName('script')[0];
+						s.parentNode.insertBefore(wf, s);
+					})();
+				</script>
+
 	</body>
 
 </html> <!-- end of site. what a ride! -->
