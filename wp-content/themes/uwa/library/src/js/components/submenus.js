dbb812cf665aa2378c21b2a46a8a5a74cb441007
David Royer <droyer01@gmail.com>
2017-05-21 13:31:36 -0400

    Updated submenus to have better aria-states

--- wp-content/themes/uwa/library/src/js/components/submenus.js
+++ wp-content/themes/uwa/library/src/js/components/submenus.js
@@ -7,42 +7,55 @@
 	const Submenus = MenuItemsWithChildren.children('.sub-menu')
 	const lastLinks = Submenus.find('a:last')
 
-	MenuLinksWithChildren
-		.after(ButtonHTML)
-		.attr('aria-expanded', false)
-		.attr('aria-haspopup', true)
+	MenuLinksWithChildren.after(ButtonHTML)
 
-	Submenus
-		.attr({
-			'aria-hidden': 'true'
-		})
+	Submenus.attr({
+		'aria-hidden': 'true'
+	})
 
 	const Buttons = $('.sub-menu__toggle')
+	Buttons.attr({
+		'aria-haspopup': true,
+		'aria-expanded': false
+	})
 
-	function submenuToggleHandler() {
-		var SubmenuButton = $(this),
-			parentListItem = SubmenuButton.parent('.menu-item-has-children'),
-			link = SubmenuButton.prev('a'),
-			newAriaExandedState = link.attr('aria-expanded') === 'false' ? true : false,
-			CurrentSubmenu = SubmenuButton.next('.sub-menu'),
-			newAriaHiddenState = CurrentSubmenu.attr('aria-hidden') === 'true' ? false : true
-
-		link.attr('aria-expanded', newAriaExandedState)
-		parentListItem.toggleClass('activeSubmenu')
-		CurrentSubmenu
-			.slideToggle(350, 'swing')
-			.attr('aria-hidden', newAriaHiddenState)
-
-		CurrentSubmenu.on('keydown', function(keyEvent) {
+	function escapeListener(submenu, submenuToggler) {
+		submenu.on('keydown', function(keyEvent) {
 			if (keyEvent.which == 27) {
-        SubmenuButton.focus()
+				submenuToggler.focus()
 			}
 		})
 	}
 
+	function toggleSubmenuState(CurrentSubmenu) {
+		let currentSubmenuButton = CurrentSubmenu.closest('.menu-item-has-children').find('.sub-menu__toggle'),
+				newAriaHiddenState = CurrentSubmenu.attr('aria-hidden') === 'true' ? false : true
+				newAriaHiddenState = CurrentSubmenu.attr('aria-hidden') === 'true' ? false : true
+				newAriaExandedState = currentSubmenuButton.attr('aria-expanded') === 'false' ? true : false
+
+		CurrentSubmenu.attr('aria-hidden', newAriaExandedState)
+		currentSubmenuButton.attr('aria-expanded', newAriaExandedState)
+	}
+
+	function slideSubmenu(CurrentSubmenu) {
+		CurrentSubmenu.slideToggle(350, 'swing', function() {
+			toggleSubmenuState(CurrentSubmenu)
+		})
+	}
+
+	function submenuToggleHandler() {
+		var SubmenuButton = $(this),
+				parentListItem = SubmenuButton.parent('.menu-item-has-children'),
+				CurrentSubmenu = SubmenuButton.next('.sub-menu')
+
+		parentListItem.toggleClass('activeSubmenu')
+		slideSubmenu(CurrentSubmenu)
+		escapeListener(CurrentSubmenu, SubmenuButton)
+	}
+
 	Buttons.on('click', submenuToggleHandler)
 
-	$('#menu-main')
+	MainMenu
 		.on('mouseenter focusin', '.sub-menu', function(e) {
 			var Current = $(this)
 			Current.attr('data-has-focus', 'true');
@@ -54,10 +67,9 @@
 				if (ActiveSubmenu.find(':focus').length) {
 					// Still in submenu
 				} else {
-					ActiveSubmenu
-						.slideToggle(350, 'swing', function() {
-							ActiveSubmenu.attr('data-has-focus', 'false');
-						});
+					ActiveSubmenu.attr('data-has-focus', 'false');
+					slideSubmenu(ActiveSubmenu)
+
 				}
 			}, 100);
 		})
