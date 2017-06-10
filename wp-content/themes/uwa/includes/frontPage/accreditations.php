e55216defe5c577c171896b77bd2da3ac9356906
David Royer <droyer01@gmail.com>
2017-06-10 19:25:37 -0400

    Update QA DOC edits

--- wp-content/themes/uwa/includes/frontPage/accreditations.php
+++ wp-content/themes/uwa/includes/frontPage/accreditations.php
@@ -1,9 +1,15 @@
 <div class="accreditations">
   <h3 class="accreditations__title">Regionally Accredited</h3>
 
-  <div class="accreditations">
-    <img src="/wp-content/themes/uwa/library/images/accreditations/1.png" alt="accreditation logo">
-    <img src="/wp-content/themes/uwa/library/images/accreditations/2.png" alt="accreditation logo">
-    <img src="/wp-content/themes/uwa/library/images/accreditations/3.png" alt="accreditation logo">
+  <div class="accreditations__logos">
+    <div class="accreditations__logoWrapper">
+      <img src="/wp-content/themes/uwa/library/images/accreditations/1.png" alt="accreditation logo">
+    </div>
+    <div class="accreditations__logoWrapper">
+      <img src="/wp-content/themes/uwa/library/images/accreditations/2.png" alt="accreditation logo">
+    </div>
+    <div class="accreditations__logoWrapper">
+      <img src="/wp-content/themes/uwa/library/images/accreditations/3.png" alt="accreditation logo">
+    </div>
   </div>
 </div>
