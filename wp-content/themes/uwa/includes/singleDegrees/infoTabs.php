b14f5f69bdf375e6664fe644a0b8f4d4f30dc207
David Royer <droyer01@gmail.com>
2017-06-10 12:44:10 -0400

    Fixed php include issue

--- wp-content/themes/uwa/includes/singleDegrees/infoTabs.php
+++ wp-content/themes/uwa/includes/singleDegrees/infoTabs.php
@@ -2,17 +2,17 @@
   <div class="infoTabs">
     <div class="infoTabs__buttons" role="tablist">
       <button id="details" class="active" role="tab" aria-selected="true" aria-controls="content-details">Course Information</button>
-      <button id="concentrations" role="tab" aria-selected="false" aria-controls="content-details">Concentrations</button>
+      <button id="concentrations" role="tab" aria-selected="false" aria-controls="content-conentrations">Concentrations</button>
       <button id="admission" role="tab" aria-selected="false" aria-controls="content-admission">Admission Requirements</button>
       <button id="tuition" role="tab" aria-selected="false" aria-controls="content-tuition">Tuition & Financing</button>
       <button id="transfer" role="tab" aria-selected="false" aria-controls="content-transfer">Transfer Credit Policy</button>
     </div>
     <div class="infoTabs__contentWrapper">
-      <?php include ('infoTabs/courseDetails.php'); ?>
-      <?php include ('infoTabs/concentrations.php'); ?>
-      <?php include ('infoTabs/admission.php'); ?>
-      <?php include ('infoTabs/tuition.php'); ?>
-      <?php include ('infoTabs/transfer.php'); ?>
+      <?php include ('courseDetails.php'); ?>
+      <?php include ('concentrations.php'); ?>
+      <?php include ('admission.php'); ?>
+      <?php include ('tuition.php'); ?>
+      <?php include ('transfer.php'); ?>
     </div>
   </div>
 </div>
