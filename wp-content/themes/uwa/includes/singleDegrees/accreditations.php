2d43a964b0c508f34c30692a78631084f0ed9417
David Royer <droyer01@gmail.com>
2017-06-22 13:46:38 -0400

    testing logo links

--- wp-content/themes/uwa/includes/singleDegrees/accreditations.php
+++ wp-content/themes/uwa/includes/singleDegrees/accreditations.php
@@ -1,9 +1,11 @@
 <div class="accreditations">
   <?php if (get_field('sacs_logo')): ?>
-    <img src="/wp-content/themes/uwa/library/images/accreditations/1.png" alt="accreditation logo">
+    <a href="http://sacs.org" target="_blank" rel="noopener">
+      <img src="/wp-content/themes/uwa/library/images/accreditations/1.png" alt="accreditation logo">
+    </a>
   <?php endif; ?>
   <?php if (get_field('alsde_logo')): ?>
-    <img src="/wp-content/themes/uwa/library/images/accreditations/2.png" alt="accreditation logo">
+    <a href="http://alsde.edu" target="_blank" rel="noopener"><img src="/wp-content/themes/uwa/library/images/accreditations/2.png" alt="accreditation logo"></a>
   <?php endif; ?>
   <?php if (get_field('ncate_logo')): ?>
     <img src="/wp-content/themes/uwa/library/images/accreditations/ncate.png" alt="accreditation logo">
