7e411d1b97a882d84ed127472824be80f21f5d38
David Royer <droyer01@gmail.com>
2017-07-07 02:15:48 -0400

    Attempt to fix underscore showing up

--- wp-content/themes/uwa/includes/singleDegrees/accreditations.php
+++ wp-content/themes/uwa/includes/singleDegrees/accreditations.php
@@ -1,8 +1,6 @@
 <div class="accreditations">
   <?php if (get_field('sacs_logo')): ?>
-    <a href="http://sacs.org" target="_blank" rel="noopener">
-      <img src="/wp-content/themes/uwa/library/images/accreditations/1.png" alt="accreditation logo">
-    </a>
+    <a href="http://sacs.org" target="_blank" rel="noopener"><img src="/wp-content/themes/uwa/library/images/accreditations/1.png" alt="accreditation logo"></a>
   <?php endif; ?>
   <?php if (get_field('alsde_logo')): ?>
     <a href="http://alsde.edu" target="_blank" rel="noopener"><img src="/wp-content/themes/uwa/library/images/accreditations/2.png" alt="accreditation logo"></a>
