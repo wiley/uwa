18a9fc6f5568a4a3570afd1975b9c28108f0b1ea
David Royer <droyer01@gmail.com>
2017-07-07 02:40:31 -0400

    Added coure info outro ACF

--- wp-content/themes/uwa/includes/singleDegrees/infoTabs/courseDetails.php
+++ wp-content/themes/uwa/includes/singleDegrees/infoTabs/courseDetails.php
@@ -9,10 +9,16 @@
     <?php include 'courseInfoAccordion.php'; ?>
   <?php endif; ?>
 
+  <?php if (get_field('course_info_intro')): ?>
+    <?php the_field('course_info_intro'); ?>
+  <?php endif; ?>
+
   <?php if( have_rows('concentration_accordion') ): $i = 0;?>
+
     <?php if (get_field('concentrations_intro')): ?>
       <div class="concentrationsIntroWrapper" style="margin-top: 3.5em;"><?php the_field('concentrations_intro'); ?></div>
     <?php endif; ?>
     <?php include 'concentrationAccordion.php'; ?>
+    
   <?php endif; ?>
 </div>
