13568ae6206c08c56021058180ccba253313dc46
David Royer <droyer01@gmail.com>
2017-07-19 13:18:07 -0400

    Removed line to remove fixedclass on resize event

--- wp-content/themes/uwa/library/src/js/scripts.js
+++ wp-content/themes/uwa/library/src/js/scripts.js
@@ -55,7 +55,7 @@ jQuery(document).ready(function($) {
 
           $(window).on('resize', function() {
               if ( stickyElement.hasClass('fixed') ) {
-                  stickyElement.removeClass('fixed');
+                  // stickyElement.removeClass('fixed');
               }
               stickyElementOffsetTop = stickyElement.offset().top;
               width = stickyElement.width() + 30
