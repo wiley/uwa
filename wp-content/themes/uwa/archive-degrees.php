bd0f8cf266c8001afbd7310f97a86bf1662413bb
David Royer <droyer01@gmail.com>
2017-07-21 14:39:24 -0400

    Filtering for degrees updated

--- wp-content/themes/uwa/archive-degrees.php
+++ wp-content/themes/uwa/archive-degrees.php
@@ -29,61 +29,68 @@ $degreeLevels = get_terms([
 	$allDegrees = get_posts( $allDegreesArgs );
 ?>
 	<main>
-		<form class="controls" id="Filters">
-  <!-- We can add an unlimited number of "filter groups" using the following format: -->
+		<div class="controlsWrapper">
+			<form class="controls" id="Filters">
+			  <!-- We can add an unlimited number of "filter groups" using the following format: -->
 
-  <fieldset>
-    <h4>Degree Types</h4>
-		<div class="degreeTypesToolbar toolbar-filter" role="toolbar">
-			<span class="toolbar-filter__label" style="font-family: 'Oswald', sans-serif; font-size: 0.9em; text-transform: uppercase;">Select Degree Level:</span>
-			<button class="btn__hollow filter" aria-label="List All Degrees Types" data-filter="all">All</button>
-				<?php foreach ($degreeTypes as $degreeType): ?>
-					<?php
-						$Name = $degreeType->name;
-						$slug = $degreeType->slug;
-					?>
+			  <fieldset>
+			    <h2 class="toolbar-filter__label">Areas Of Study</h2>
+					<div class="degreeTypesToolbar toolbar-filter" role="toolbar">
+						<!-- <span class="toolbar-filter__label" style="font-family: 'Oswald', sans-serif; font-size: 0.9em; text-transform: uppercase;">Select Degree Level:</span> -->
+						<button class="btn__hollow filter" aria-label="List All Degrees Types" data-filter="all">All</button>
+							<?php foreach ($degreeTypes as $degreeType): ?>
+								<?php
+									$Name = $degreeType->name;
+									$slug = $degreeType->slug;
+								?>
 
-						<button class="btn__hollow filter" aria-label="Filter By <?php echo $Name; ?>" data-filter=".<?php echo $slug; ?>"><?php echo $Name; ?></button>
-				<?php endforeach; ?>
+									<button class="btn__hollow filter" aria-label="Filter By <?php echo $Name; ?>" data-filter=".<?php echo $slug; ?>"><?php echo $Name; ?></button>
+							<?php endforeach; ?>
+					</div>
+			  </fieldset>
+
+			  <fieldset>
+			    <h2 class="toolbar-filter__label">Degree Types</h2>
+					<div class="degreeLevelsToolbar toolbar-filter" role="toolbar">
+						<!-- <span class="toolbar-filter__label" style="font-family: 'Oswald', sans-serif; font-size: 0.9em; text-transform: uppercase;">Select Degree Level:</span> -->
+						<button  class="btn__hollow filter" aria-label="List All Degrees Levels" data-filter="all">All</button>
+							<?php foreach ($degreeLevels as $degreeLevel): ?>
+								<?php
+									$Name = $degreeLevel->name;
+									$slug = $degreeLevel->slug;
+								?>
+
+									<button  class="btn__hollow filter" aria-label="Filter By <?php echo $Name; ?>" data-filter=".<?php echo $slug; ?>"><?php echo $Name; ?></button>
+							<?php endforeach; ?>
+					</div>
+			  </fieldset>
+
+			  <button id="Reset" class="btn-red">Clear Filters</button>
+			</form>
 		</div>
-  </fieldset>
 
-  <fieldset>
-    <h4>Degree Levels</h4>
-		<div class="degreeLevelsToolbar toolbar-filter" role="toolbar">
-			<span class="toolbar-filter__label" style="font-family: 'Oswald', sans-serif; font-size: 0.9em; text-transform: uppercase;">Select Degree Level:</span>
-			<button  class="btn__hollow filter" aria-label="List All Degrees Levels" data-filter="all">All</button>
-				<?php foreach ($degreeLevels as $degreeLevel): ?>
-					<?php
-						$Name = $degreeLevel->name;
-						$slug = $degreeLevel->slug;
-					?>
+			<div class="mix-containerWrapper">
+<!-- <div class="wrap cf"> -->
+				<ul id="mix-container" class="container noList cf">
+					<?php foreach ($allDegrees as $post): ?>
+						<?php setup_postdata($post); ?>
+							<?php $terms = get_the_terms( $post->ID, 'degree_vertical'); ?>
 
-						<button  class="btn__hollow filter" aria-label="Filter By <?php echo $Name; ?>" data-filter=".<?php echo $slug; ?>"><?php echo $Name; ?></button>
-				<?php endforeach; ?>
-		</div>
-  </fieldset>
+							<a style="background-image: url(<?php the_field('program_image'); ?>);" class="mix card <?php echo custom_taxonomies_terms_slugs(); ?>" href="<?php the_permalink(); ?>">
+								<div class="card__infoWrapper">
+									<?php if (!empty( $terms )): ?>
+									<?php endif; ?>
+									<h4 class="card__title"><?php the_title(); ?></h4>
 
-  <button id="Reset">Clear Filters</button>
-</form>
-
-			<ul id="mix-container" class="container noList cf">
-				<?php foreach ($allDegrees as $post): ?>
-					<?php setup_postdata($post); ?>
-						<?php $terms = get_the_terms( $post->ID, 'degree_vertical'); ?>
-						<a class="mix card <?php echo custom_taxonomies_terms_slugs(); ?>" href="<?php the_permalink(); ?>">
-							<div>
-								<?php if (!empty( $terms )): ?>
-								<?php endif; ?>
-								<h4><?php the_title(); ?></h4>
-
-								<div class="card__info">More Information &gt;&gt;</div>
-							</div>
-						</a>
-				<?php endforeach;?>
+									<div class="card__info">More Information &gt;&gt;</div>
+								</div>
+							</a>
+					<?php endforeach;?>
 
 
-			</ul>
+				</ul>
+<!-- </div> -->
+			</div>
 
 		<!-- <div id="Container" class="container">
 		  <div class="mix triangle white"></div>
