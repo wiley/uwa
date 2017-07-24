a1dc3db339aef87ca9486cbde2f9d629a16eef13
David Royer <droyer01@gmail.com>
2017-07-24 09:51:00 -0400

    Update links

--- wp-content/themes/uwa/includes/megamenu.php
+++ wp-content/themes/uwa/includes/megamenu.php
@@ -1,8 +1,6 @@
 <script id="degrees-menu-template" type="text/template">
   <?php
-  // your taxonomy name
     $verticalsTaxonomy = 'degree_vertical';
-    // get the terms of taxonomy
     $degreeTypes = get_terms( $verticalsTaxonomy, $args = array(
       'hide_empty' => false, // do not hide empty terms
     ));
@@ -17,22 +15,6 @@
     ));
   ?>
 
-  <?php
-  // loop through all terms
-  // foreach( $degreeTypes as $type ) {
-  //
-  //     // Get the term link
-  //     $type_link = get_term_link( $type );
-  //
-  //     if( $type->count > 0 )
-  //         // display link to term archive
-  //         echo '<a href="' . esc_url( $type_link ) . '">' . $type->name .'</a>';
-  //
-  //     elseif( $type->count !== 0 )
-  //         // display name
-  //         echo '' . $type->name .'';
-  // }
-  ?>
 
     <ul id="" class="degrees-menu sub-menu">
       <ul class="degrees-menu__submenu">
@@ -55,9 +37,6 @@
           ?>
           <li><a href="<?php echo esc_url( $url ); ?>"><?php echo $name; ?></a></li>
         <?php endforeach; ?>
-        <!-- <li><a href="#">Business</a></li>
-        <li><a href="#">Teaching</a></li>
-        <li><a href="#">Psychology & Counseling</a></li> -->
       </ul>
     </ul>
 </script>
