06fcc08f00ba8c301c7080b7d1da45d6e9eae687
David Royer <droyer01@gmail.com>
2017-07-26 13:26:15 -0400

    logoHolder styles

--- wp-content/themes/uwa/includes/frontPage/accreditations.php
+++ wp-content/themes/uwa/includes/frontPage/accreditations.php
@@ -3,13 +3,13 @@
 
   <div class="accreditations__logos">
     <div class="accreditations__logoWrapper">
-      <img src="/wp-content/themes/uwa/library/images/accreditations/1.png" alt="accreditation logo">
+      <a href="http://sacs.org/" target="_blank" rel="nofollow"><img src="/wp-content/themes/uwa/library/images/accreditations/1.png" alt="accreditation logo"></a>
     </div>
     <div class="accreditations__logoWrapper">
-      <img src="/wp-content/themes/uwa/library/images/accreditations/2.png" alt="accreditation logo">
+    <a href="http://alsde.edu/" target="_blank" rel="nofollow"><img src="/wp-content/themes/uwa/library/images/accreditations/2.png" alt="accreditation logo"></a>
     </div>
     <div class="accreditations__logoWrapper">
-      <img src="/wp-content/themes/uwa/library/images/accreditations/ncate.png" alt="accreditation logo">
+      <a href="http://ncate.org/" target="_blank" rel="nofollow"><img src="/wp-content/themes/uwa/library/images/accreditations/ncate.png" alt="accreditation logo"></a>
     </div>
   </div>
 </div>
