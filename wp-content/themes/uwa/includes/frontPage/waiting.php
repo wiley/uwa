04513cc3b86fc72709dc340d3da62b8dd0f96636
David Royer <droyer01@gmail.com>
2017-08-01 13:25:56 -0400

    Update

--- wp-content/themes/uwa/includes/frontPage/waiting.php
+++ wp-content/themes/uwa/includes/frontPage/waiting.php
@@ -2,6 +2,6 @@
   <div class="wrap cf">
     <h2 class="waiting__heading section-heading">What are you waiting for?</h2>
     <a href="/request-info" class="btn-red">Request Info</a>
-    <a href="#" class="btn-red">Apply Now</a>
+    <a href="https://www.gowest-alabama.org/apply/get-started.htm" target="_blank" rel="nofollow" class="btn-red">Apply Now</a>
   </div>
 </div>
