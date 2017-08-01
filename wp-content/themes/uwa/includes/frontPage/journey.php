dc32434035a09f2be7596baa925a484007e21e59
David Royer <droyer01@gmail.com>
2017-08-01 11:47:03 -0400

    Updates

--- wp-content/themes/uwa/includes/frontPage/journey.php
+++ wp-content/themes/uwa/includes/frontPage/journey.php
@@ -5,17 +5,17 @@
     <div class="card">
       <h3 class="card__heading">Business</h3>
       <p class="card__text">Earn a competitive degree in fields such as accounting, marketing, management and more.</p>
-      <a class="card__link btn btn-info" href="/areas-of-styudy/business"  href="#">Business Degrees</a>
+      <a class="card__link btn btn-info" href="/online-degrees/areas-of-study/business"  href="#">Business Degrees</a>
     </div>
     <div class="card">
       <h3 class="card__heading">Teaching</h3>
       <p class="card__text">Progress as an educator in and outside the classroom with a wide range of programs.</p>
-      <a class="card__link btn btn-info" href="/areas-of-styudy/teaching">Teaching Degrees</a>
+      <a class="card__link btn btn-info" href="/online-degrees/areas-of-study/teaching">Teaching Degrees</a>
     </div>
     <div class="card">
       <h3 class="card__heading">Psychology & Counseling</h3>
       <p class="card__text">Pursue an advanced career in clinical mental health counseling, experimental psychology and more.</p>
-      <a class="card__link btn btn-info" href="/areas-of-styudy/psychology-counseling">Psychology Degrees</a>
+      <a class="card__link btn btn-info" href="/online-degrees/areas-of-study/psychology-counseling">Psychology Degrees</a>
     </div>
   </div>
   <p class="journey__extraText">Other programs include interdisciplinary studies, conservation biology and library media. <a href="/online-degrees">VIEW ALL DEGREES</a>
