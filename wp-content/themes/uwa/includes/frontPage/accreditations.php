fa9fafd0c85dc317cf0ae4d9125c78f42c12af8f
David Royer <droyer01@gmail.com>
2017-08-18 12:05:46 -0400

    Accreditations

--- wp-content/themes/uwa/includes/frontPage/accreditations.php
+++ wp-content/themes/uwa/includes/frontPage/accreditations.php
@@ -1,5 +1,5 @@
 <div class="accreditations">
-  <h3 class="accreditations__title">Regionally Accredited</h3>
+  <h3 class="accreditations__title">Accreditation</h3>
 
   <div class="accreditations__logos">
     <div class="accreditations__logoWrapper">
@@ -10,6 +10,12 @@
     </div>
     <div class="accreditations__logoWrapper">
       <a href="http://ncate.org/" target="_blank" rel="nofollow"><img src="/wp-content/themes/uwa/library/images/accreditations/ncate.png" alt="accreditation logo"></a>
+    </div>
+    <div class="accreditations__logoWrapper">
+      <a href="http://www.acbsp.org/?page=search_accredited" target="_blank" rel="nofollow"><img src="/wp-content/themes/uwa/library/images/accreditations/acbsp.png" alt="accreditation logo"></a>
+    </div>
+    <div class="accreditations__logoWrapper">
+      <a href="http://caepnet.org/" target="_blank" rel="nofollow"><img src="/wp-content/themes/uwa/library/images/accreditations/caep.png" alt="accreditation logo"></a>
     </div>
   </div>
 </div>
