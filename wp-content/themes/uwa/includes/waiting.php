f36d5ae551b839c448b2a213e6a037a8c9057728
Chelsea Wadsworth <Chelsea Wadsworth>
2018-09-04 14:18:14 -0400

    update apply link

--- wp-content/themes/uwa/includes/waiting.php
+++ wp-content/themes/uwa/includes/waiting.php
@@ -2,6 +2,6 @@
   <div class="wrap cf">
     <h2 class="waiting__heading section-heading">What are you waiting for?</h2>
     <a href="/request-info" class="btn-red">Request Info</a>
-    <a class="btn-red" target="_blank" rel="noopener noreferrer" href="https://www.gowest-alabama.org/apply/get-started.htm">Apply Now</a>
+    <a class="btn-red" target="_blank" rel="noopener noreferrer" href="https://www.gowest-alabama.org/freshman/get-started.htm">Apply Now</a>
   </div>
 </div>
