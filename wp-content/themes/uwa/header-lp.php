2eb81391cffab4ade8172f093536586fcdc04198
David Royer <droyer01@gmail.com>
2020-03-30 12:42:38 -0400

    Update header-lp.php

--- wp-content/themes/uwa/header-lp.php
+++ wp-content/themes/uwa/header-lp.php
@@ -42,15 +42,6 @@
 
 	<body <?php body_class(); ?> itemtype="http://schema.org/WebPage" itemscope>
 
-		<!-- Google Tag Manager -->
-		<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-		new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-			j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-			'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-		})(window,document,'script','dataLayer','GTM-WWK9WWK');</script>
-		<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WWK9WWK"
-		height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-		<!-- End Google Tag Manager -->
 
 		<div class="container">
 
