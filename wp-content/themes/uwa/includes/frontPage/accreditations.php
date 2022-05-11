0c8312b9872ab9a41c631e633327dfba8b7a0106
ssabaripriyatharshini-se <sseevaratn@wiley.com>
2022-05-11 13:29:41 +0530

    fix(mtik-4607): removed accreditation logo links

--- wp-content/themes/uwa/includes/frontPage/accreditations.php
+++ wp-content/themes/uwa/includes/frontPage/accreditations.php
@@ -27,16 +27,16 @@
 			else:
 		?>
     <div class="accreditations__logoWrapper">
-      <a href="http://sacs.org/" target="_blank" rel="nofollow"><img src="/wp-content/themes/uwa/library/images/accreditations/1.png" alt="accreditation logo"></a>
+      <img src="/wp-content/themes/uwa/library/images/accreditations/1.png" alt="accreditation logo">
     </div>
     <div class="accreditations__logoWrapper">
-    <a href="http://alsde.edu/" target="_blank" rel="nofollow"><img src="/wp-content/themes/uwa/library/images/accreditations/2.png" alt="accreditation logo"></a>
+    <img src="/wp-content/themes/uwa/library/images/accreditations/2.png" alt="accreditation logo">
     </div>
    <!-- <div class="accreditations__logoWrapper">
       <a href="http://www.acbsp.org/?page=search_accredited" target="_blank" rel="nofollow"><img src="/wp-content/themes/uwa/library/images/accreditations/acbsp.png" alt="accreditation logo"></a>
     </div> -->
     <div class="accreditations__logoWrapper">
-      <a href="http://caepnet.org/" target="_blank" rel="nofollow"><img src="/wp-content/themes/uwa/library/images/accreditations/caep.png" alt="accreditation logo"></a>
+      <img src="/wp-content/themes/uwa/library/images/accreditations/caep.png" alt="accreditation logo">
     </div>
 		<?php endif; ?>
   </div>
--- wp-content/themes/uwa/includes/singleDegrees/accreditations.php
+++ wp-content/themes/uwa/includes/singleDegrees/accreditations.php
@@ -1,20 +1,20 @@
 <div class="accreditations">
   <?php if (get_field('sacs_logo')): ?>
-    <a href="http://sacs.org" target="_blank" rel="noopener"><img src="/wp-content/themes/uwa/library/images/accreditations/1.png" alt="accreditation logo"></a>
+    <img src="/wp-content/themes/uwa/library/images/accreditations/1.png" alt="accreditation logo">
   <?php endif; ?>
   <?php if (get_field('alsde_logo')): ?>
-    <a href="http://alsde.edu" target="_blank" rel="noopener"><img src="/wp-content/themes/uwa/library/images/accreditations/2.png" alt="accreditation logo"></a>
+    <img src="/wp-content/themes/uwa/library/images/accreditations/2.png" alt="accreditation logo">
   <?php endif; ?>
   <?php if (get_field('ncate_logo')): ?>
-    <a href="http://ncate.org" target="_blank" rel="noopener"><img src="/wp-content/themes/uwa/library/images/accreditations/ncate.png" alt="accreditation logo"></a>
+    <img src="/wp-content/themes/uwa/library/images/accreditations/ncate.png" alt="accreditation logo">
   <?php endif; ?>
   <?php if (get_field('caep_logo')): ?>
-    <a href="http://caepnet.org/" target="_blank" rel="noopener"><img src="/wp-content/themes/uwa/library/images/accreditations/caep.png" alt="accreditation logo"></a>
+    <img src="/wp-content/themes/uwa/library/images/accreditations/caep.png" alt="accreditation logo">
   <?php endif; ?>
   <?php if (get_field('acbsp_logo')): ?>
-    <a href="http://www.acbsp.org/?page=search_accredited" target="_blank" rel="noopener"><img src="/wp-content/themes/uwa/library/images/accreditations/acbsp.png" alt="accreditation logo"></a>
+    <img src="/wp-content/themes/uwa/library/images/accreditations/acbsp.png" alt="accreditation logo">
   <?php endif; ?>
   <?php if (get_field('oer_logo')): ?>
-    <a href="#oer_module" target="_blank" rel="noopener"><img src="/wp-content/uploads/2019/02/OER-Badge-Vert-UWA.png"></a>
+    <img src="/wp-content/uploads/2019/02/OER-Badge-Vert-UWA.png">
   <?php endif; ?>
 </div>
